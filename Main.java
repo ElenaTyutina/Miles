213d20c1c33515005dd27f94fa52f56b35ebb5e6
Elena Tuytina <yptyutina@mail.ru>
2021-07-21 18:11:37 +0300

    Miles

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 5000;
+        int miles = 1;
+        int milesCost = 20;
+        int bonus = ticket * miles / milesCost;
+        System.out.println(bonus);
+
+    }
+}
+
+
